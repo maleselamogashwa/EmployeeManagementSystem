71c1b7228c255580cd022baaf97d45fc2a7ecb74
malesela <malesela3@gmail.com>
2021-02-08 15:59:08 +0200

    classes updated

--- EmployeeManagement/src/Customer.java
+++ EmployeeManagement/src/Customer.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Customer {
+public class Customer {
+//Place an order
+//View order details
+//Cancel pending order
+//View previous order transaction
+    private int customerID;
+    private String customerName;
+    private String company;
+    private int orderID;
+
+    public Customer(int customerID, int orderID){
+        this.customerID = customerID;
+        this.orderID = orderID;
+        
+    }
+
 }
--- EmployeeManagement/src/Warehouse.java
+++ EmployeeManagement/src/Warehouse.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
+
 public class Warehouse {
-    private String goods;
-    private int goodsBarCode;
-    private double goodsCostPerUnit;
-    private String goodsClassification;
+//view products in stock
+//add products to current stock
+//remove products from stock
+//set price of product
+
+    private String productName;
+    private String productBarcode;
+    private double productPrice;
+    private int productQuantity;
+    private int productLabel;
+
+    public  Warehouse(String productName){
+        this.productName = productName;
+        this.productBarcode = "000";
+        this.productPrice = 0.0;
+        this.productQuantity = 0;
+        this.productLabel = 0;
+    }
+
+    public void setProductLabel(int classCode){
+        System.out.println("Enter product classification: 1. Perishable goods/n2. Non-perishable goods/n3. Dangerous goods/n4. Minerals/n5. Chemicals");
+        Scanner sc = new Scanner(System.in);
+        classCode = sc.nextInt();
+        productLabel = classCode;
+    }
+
+    public void setProductBarcode(){
+        String productCode = productLabel + productBarcode;
+    }
+
+    public String getName(String productName){
+        return productName;
+    }
+
+    public double setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+        return productPrice;
+    }
 }
